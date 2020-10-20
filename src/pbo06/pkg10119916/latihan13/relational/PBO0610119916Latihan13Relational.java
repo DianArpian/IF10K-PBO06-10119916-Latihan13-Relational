4febc4837e77eb15b7b93ad4a669d3782cf29fa6
DianArpian <>
2020-10-20 20:26:21 +0700

    Relational

---
+++ src/pbo06/pkg10119916/latihan13/relational/PBO0610119916Latihan13Relational.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo06.pkg10119916.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * NAMA     : DIAN ARPIAN
+ * KELAS    : IF10K
+ * NIM      : 10119916 
+ * Deskripsi Program : Relational
+ */
+public class PBO0610119916Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        int bil1 = 10;
+        int bil2 = 20;
+        
+        System.out.println("a == b = " + (bil1 == bil2));
+         System.out.println("a != b = " + (bil1 != bil2));
+          System.out.println("a > b = " + (bil1 > bil2));
+           System.out.println("a < b = " + (bil1 < bil2));
+            System.out.println("b >= a = " + (bil2 >= bil1));
+             System.out.println("b <= a = " + (bil2 <= bil1));
+             
+    }
+    
+}
